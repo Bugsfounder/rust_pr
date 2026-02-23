fcada836999b3d03d1d2aa4c22bf368c61084b08
bugsfounder <bugsfounder2021@gmail.com>
2026-02-23 11:43:03 +0530

    getting started with rust

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-80384ad008851c2d.d
@@ -0,0 +1,5 @@
+/home/bugsfounder/workspace/rust_pr/projects/hello_cargo/target/debug/deps/hello_cargo-80384ad008851c2d.d: src/main.rs
+
+/home/bugsfounder/workspace/rust_pr/projects/hello_cargo/target/debug/deps/hello_cargo-80384ad008851c2d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/bugsfounder/workspace/rust_pr/projects/hello_cargo/target/debug/hello_cargo: /home/bugsfounder/workspace/rust_pr/projects/hello_cargo/src/main.rs
